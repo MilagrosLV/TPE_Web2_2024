c25285ec47bf3b1bc1eb7bf4302ac0025429328a
Milagros Lopez Vilaclara <lopezvmilagros@gmail.com>
2024-09-25 16:48:26 -0300

    Modif DB

--- app/productos.model.php
+++ app/productos.model.php
@@ -2,7 +2,7 @@
 require_once './productos.controller.php';
 
 function obtenerProductosPorCategoria($categoria){
-    $db = new PDO('mysql:host=localhost;' . 'dbname=supermercado;charset=utf8', 'root', '');
+    $db = new PDO('mysql:host=localhost;' . 'dbname=db_supermercado;charset=utf8', 'root', '');
     $query = $db->prepare('SELECT * FROM productos WHERE Categoria = ?');
     $query->execute([$categoria]);
     $productos = $query->fetchAll(PDO::FETCH_OBJ);
