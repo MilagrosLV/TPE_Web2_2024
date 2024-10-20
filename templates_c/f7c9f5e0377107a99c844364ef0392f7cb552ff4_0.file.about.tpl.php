f2dc1588dd7e85ad396587f0ffaa4f107149ff9c
Milagros Lopez Vilaclara <lopezvmilagros@gmail.com>
2024-10-20 19:09:16 -0300

    templates_c

---
+++ templates_c/f7c9f5e0377107a99c844364ef0392f7cb552ff4_0.file.about.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 4.3.1, created on 2024-10-21 00:08:26
+  from 'C:\xampp\htdocs\web2\TPE_Web2_2024\templates\about.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_67157f5acbccf1_25955600',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f7c9f5e0377107a99c844364ef0392f7cb552ff4' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\web2\\TPE_Web2_2024\\templates\\about.tpl',
+      1 => 1729381335,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:header.tpl' => 1,
+    'file:footer.tpl' => 1,
+  ),
+),false)) {
+function content_67157f5acbccf1_25955600 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+<div class="content">
+    <br>
+    <h3>Estudiantes</h3>
+    <ul>
+        <li>López Vilaclara, Milagros.</li>
+        <li>Saffarano, Tomás.</li>
+    </ul>
+</div>
+<?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
---
+++ templates_c/ffb0dd274824831385e9fbc12c4ff32de520cf99_0.file.footer.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 4.3.1, created on 2024-10-21 00:07:51
+  from 'C:\xampp\htdocs\web2\TPE_Web2_2024\templates\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_67157f3726cf58_75260135',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'ffb0dd274824831385e9fbc12c4ff32de520cf99' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\web2\\TPE_Web2_2024\\templates\\footer.tpl',
+      1 => 1729357561,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67157f3726cf58_75260135 (Smarty_Internal_Template $_smarty_tpl) {
+?>        <footer>
+            <p class="footer">WEB 2 - TPE 2024</p>
+        </footer>
+    </body>
+</html><?php }
+}
